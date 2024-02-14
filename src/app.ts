89a36fe7a12ecf9a3e42bf9be161db5c06f9ffd4
SAI SATHWIK REDDY VARIKOTI <varikotisaisathwikreddy@gmail.com>
2024-02-14 03:13:16 -0500

    signup endpoint complete with duo authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express, { Express, Request, Response } from "express";
-// import cors from 'cors';
+import express, { Express } from "express";
+import cors from "cors";
 
 // Assuming approutes is properly typed in its own TypeScript file
-// import approutes from './routes/approutes';
+import router from "./routes";
+import session from "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    duo?: { state: string; username: string };
+  }
+}
 
 const app: Express = express();
 const port: number = 8080;
@@ -11,10 +18,15 @@ app.use(express.json());
 app.use(express.urlencoded({
 	extended: true,
 }));
+app.use(session({
+	secret: "super-secret-phrase",
+	resave: false,
+	saveUninitialized: true
+}));
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(approutes);
+app.use(router);
 
 app.listen(port, (): void => {
 	console.log(`Server is running: ${port}`);
