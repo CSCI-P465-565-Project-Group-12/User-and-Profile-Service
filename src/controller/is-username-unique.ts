d440d3f73cf9d61f2a8f6c5e04d178d0019c1344
SAI SATHWIK REDDY VARIKOTI <varikotisaisathwikreddy@gmail.com>
2024-02-14 03:15:14 -0500

    isusernameunique endpoint complete

--- src/controller/is-username-unique.ts
+++ src/controller/is-username-unique.ts
@@ -1,28 +1,22 @@
-import express, {Request, Response, NextFunction} from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Request, Response, NextFunction } from "express";
+import { checkUserNameAvailability } from "../db/users-db";
 
-const prisma = new PrismaClient();
 
 // Route to check if a username exists
 export const isUsernameUnique = async (req:Request, res:Response) => {
-  const { username } = req.query; // Using query parameters for simplicity
-  if (!username) {
-    return res.status(400).json({ message: "Please provide a username." });
-  }
-  try {
-    const user = await prisma.users.findUnique({
-      where: {
-        username: String(username),
-      },
-    });
-
-    if (user) {
-      res.json({ exists: true, message: "Username already exists." });
-    } else {
-      res.json({ exists: false, message: "Username is available." });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "An error occurred while checking the username." });
-  }
+	const { username } = req.query || ''; // Using query parameters for simplicity
+	if (!username) {
+		return res.status(400).json({ message: "Please provide a username." });
+	}
+	try {
+		const user = await checkUserNameAvailability(username.toString());
+		if (user) {
+			res.json({ exists: false, message: "Username already exists." });
+		} else {
+			res.json({ exists: true, message: "Username is available." });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "An error occurred while checking the username." });
+	}
 };
